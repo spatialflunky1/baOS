2fe7a24978d94b636bd0a59c5ea654639e7426b9
Spatialflunky1 <mjandrus06@gmail.com>
2025-02-14 14:41:24 -0500

    Removed test code

--- src/baos/src/kernel/main.c
+++ src/baos/src/kernel/main.c
@@ -8,10 +8,6 @@ int main(KERNEL_BOOT_INFO* BootInfo) {
     
     printf("baOS kernel successfully loaded\n"); 
   
-    while (true) {
-        printf("123\n");
-        printf("1234556\n");
-    }
     while(1);
     return 0;
 }
